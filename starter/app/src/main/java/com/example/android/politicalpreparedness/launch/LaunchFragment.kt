71f08c7b8597dc7bae6d080507e56c3324904c54
miro <minev.miro@gmail.com>
2021-03-20 15:48:49 +0200

    implement launch screen with navigation

--- starter/app/src/main/java/com/example/android/politicalpreparedness/launch/LaunchFragment.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/launch/LaunchFragment.kt
@@ -1,27 +1,24 @@
 package com.example.android.politicalpreparedness.launch
 
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.example.android.politicalpreparedness.R
 import com.example.android.politicalpreparedness.databinding.FragmentLaunchBinding
-import com.example.android.politicalpreparedness.election.adapter.ElectionListAdapter
-import com.example.android.politicalpreparedness.election.adapter.ElectionListener
 
 class LaunchFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         val binding = FragmentLaunchBinding.inflate(inflater)
         binding.lifecycleOwner = this
-
-        binding.representativeButton.setOnClickListener { navToRepresentatives() }
-        binding.upcomingButton.setOnClickListener { navToElections() }
-
+        binding.buttonElections.setOnClickListener { navToElections() }
+        binding.buttonRepresentatives.setOnClickListener { navToRepresentatives() }
         return binding.root
     }
 
@@ -32,5 +29,4 @@ class LaunchFragment : Fragment() {
     private fun navToRepresentatives() {
         this.findNavController().navigate(LaunchFragmentDirections.actionLaunchFragmentToRepresentativeFragment())
     }
-
 }
