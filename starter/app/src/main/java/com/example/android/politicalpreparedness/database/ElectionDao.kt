2b3866cc00fcbb09431a96b961fcce0b1e8623b7
miro <minev.miro@gmail.com>
2021-03-21 14:16:56 +0200

    add android test for dao

--- starter/app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
@@ -1,5 +1,6 @@
 package com.example.android.politicalpreparedness.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.example.android.politicalpreparedness.network.models.Election
 
@@ -8,6 +9,40 @@ import com.example.android.politicalpreparedness.network.models.Election
  */
 @Dao
 interface ElectionDao {
+
+    /**
+     * Observes list of elections.
+     *
+     * @return all elections.
+     */
+    @Query("SELECT * FROM election_table")
+    fun observeElections(): LiveData<List<Election>>
+
+    /**
+     * Observes a single election.
+     *
+     * @param electionId the election id.
+     * @return the election with electionId.
+     */
+    @Query("SELECT * FROM election_table WHERE id = :electionId")
+    fun observeElectionById(electionId: Int): LiveData<Election>
+
+    /**
+     * Select all elections from the elections table.
+     *
+     * @return all elections.
+     */
+    @Query("SELECT * FROM election_table")
+    suspend fun getElections(): List<Election>
+
+    /**
+     * Select an election by id.
+     *
+     * @param electionId the election id.
+     * @return the election with electionId.
+     */
+    @Query("SELECT * FROM election_table WHERE id = :electionId")
+    suspend fun getElectionById(electionId: Int): Election?
 
     /**
      * Insert an election in the database. If the election already exists, replace it.
